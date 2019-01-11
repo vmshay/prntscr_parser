d405c5cc80b4749bee3abdedd0c5bfcb1d9665d0
vmshay <37113778+vmshay@users.noreply.github.com>
2019-01-11 17:11:26 +0700

    Add files via upload

---
+++ Prntsc_Scraper.ps1
@@ -0,0 +1,34 @@
+﻿$Fail=0;
+$count = 0;
+While($true)
+{
+
+    function URIGen(){
+    $arr ='a','b','c','d','e','f',
+        'g','h','i','j','k','l',
+        'm','n','o','p','r','s',
+        't','u','v','x','y','z',
+        '1','2','3','4','5','6',
+        '7','8','9','0';
+
+    $pass = "";
+        for($i = 0; $i -lt 6; $i++)
+        {
+        $index = Get-Random -Minimum 0 -Maximum ($arr.Length-1);
+        $pass += $arr[$index];
+        }
+        return $pass;
+    }
+
+
+$URI = URIGen;
+Try {wget -Uri (curl https://prnt.sc/$URI).Images[0].src -OutFile "F:\prtscr\$URI.jpg"}
+Catch { $Fail += 1;}
+$count +=1;
+}
+
+
+
+#Fails
+#378/1000
+#3731000
